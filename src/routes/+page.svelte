77ef271cc8b96ee0ce390987f7fa05120f32b38f
Jana Rajakumar <jana.rajakumar@utoronto.ca>
2023-02-09 11:24:20 -0500

    Add music and repeating cover image

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,14 +2,18 @@
   import { onMount } from 'svelte'
   import { slide } from 'svelte/transition'
 
-  const TOTAL_IMAGES = 20
+  const TOTAL_IMAGES = 35
   const IMAGES = new Array(TOTAL_IMAGES).fill(0).map((_, i) => i)
-  let activeImage = 1
+  let activeImage = 0
+  let player = null
+  let showCoverImage = false
 
   // loop through images every 5 seconds and update activeImage
   onMount(() => {
     const internal = setInterval(() => {
       activeImage = (activeImage + 1) % TOTAL_IMAGES
+      // every 5th image, show the cover image
+      showCoverImage = activeImage % 5 === 0
     }, 6000)
     return () => clearInterval(internal)
   })
@@ -27,15 +31,28 @@
       d.exitFullscreen || d.webkitExitFullscreen || d.mozCancelFullScreen || d.msExitFullscreen
     if (!d.fullscreenElement) {
       rfs.call(el)
+      player.play()
     } else {
       cfs.call(d)
+      player.pause()
     }
   }
 </script>
 
+<audio loop bind:this={player}>
+  <source src="/music.mp3" type="audio/mpeg" />
+</audio>
+
 <button on:click={goFullscreen}>
   {#each IMAGES as i}
-    {#if i === activeImage}
+    {#if showCoverImage}
+      <img
+        transition:slide={{ delay: 1000, duration: 1000 }}
+        src="/images/0-min.jpg"
+        class="h-screen w-screen object-fill"
+        alt=""
+      />
+    {:else if i === activeImage}
       <img
         transition:slide={{ delay: 1000, duration: 1000 }}
         src="/images/{activeImage}-min.jpg"
