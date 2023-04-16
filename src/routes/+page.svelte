3f674d914047a2eecfcad3c1fa9dc6f59a9bff42
Jana Rajakumar <jana.rajakumar@utoronto.ca>
2023-04-16 17:44:08 -0400

    Use compressed video

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -23,13 +23,7 @@
 </script>
 
 <button on:click={goFullscreen}>
-  <iframe
-    width="560"
-    height="315"
-    src="https://www.youtube.com/embed/lMBTyCzuLKc?controls=0"
-    title="YouTube video player"
-    frameborder="0"
-    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-    allowfullscreen
-  />
+  <video loop bind:this={player} src="/video.mp4" class="h-screen w-screen">
+    <track kind="captions" />
+  </video>
 </button>
